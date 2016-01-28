5400abd9518323e4cf2936cfe6f27c4d95e0937e
AtomicBlom <atomicblom@binaryvibrance.net>
2016-01-29 00:31:33 +1030

    Now trying JSON

--- routes/bamboo-proxy.js
+++ routes/bamboo-proxy.js
@@ -44,12 +44,24 @@ router.post("/", function(req, res) {
     try {
         if (indexOf.call(eventTypes, eventType) >= 0) {
             if (auto_trigger) {
-                bamboo_uri = bamboo_url + "/rest/api/latest/queue/" + build_key + "?bamboo.variable.pull_ref=" + data.pull_request.head.ref + "%26bamboo.variable.pull_sha=" + data.pull_request.head.sha + "%26bamboo.variable.pull_num=" + data.number;
+                //bamboo_uri = bamboo_url + "/rest/api/latest/queue/" + build_key + "?bamboo.variable.pull_ref=" + data.pull_request.head.ref + "%26bamboo.variable.pull_sha=" + data.pull_request.head.sha + "%26bamboo.variable.pull_num=" + data.number;
+                bamboo_uri = bamboo_url + "/rest/api/latest/queue/" + build_key;
+                //+ "?bamboo.variable.pull_ref=" + data.pull_request.head.ref + "%26bamboo.variable.pull_sha=" + data.pull_request.head.sha + "%26bamboo.variable.pull_num=" + data.number;
+                var bambooVariables = {
+                    'bamboo.variable.pull_ref': data.pull_request.head.ref,
+                    'bamboo.variable.pull_sha': data.pull_request.head.sha,
+                    'bamboo.variable.pull_num': data.number
+                };
 
-                var headers = { Authorization: "Basic " + auth};
+                var headers = {
+                    Authorization: "Basic " + auth,
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                };
                 console.log("Invoking " + bamboo_uri);
-                console.log("Headers: " + headers)
-                fetch(bamboo_uri, { method: 'POST', headers: headers })
+                console.log(headers);
+                console.log(bambooVariables);
+                fetch(bamboo_uri, { method: 'POST', body: JSON.stringify(bambooVariables), headers: headers })
                     .catch(function(rejection) {
                         return console.log("Encountered an error sending to bamboo " + rejection);
                     })
